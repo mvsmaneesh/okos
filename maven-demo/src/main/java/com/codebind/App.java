4cd86473ce64e22135427ae4a86653e84d88aec7
mvsmaneesh <maneesh.morampudi@flexagon.com>
2019-05-14 14:42:12 +0530

    first commit

---
+++ maven-demo/src/main/java/com/codebind/App.java
@@ -0,0 +1,13 @@
+package com.codebind;
+
+public class App {
+	public String sample()
+	{
+		return "sample";
+	}
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+	}
+
+}
